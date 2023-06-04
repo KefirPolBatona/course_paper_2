cba8c1295df1738275524359fdfa6ed1902eb186
evgeni <9232485@gmail.com>
2023-06-05 01:34:54 +0300

    ready

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import json
 
 def list_operations():
     """
-    Loads a list of banking transactions
+    Загружает список банковских операций
     """
     with open("../data/operations.json", encoding="utf-8") as file:
         return json.load(file)
--- tests/test_.py
+++
@@ -1,3 +0,0 @@
-import
-
-assert len(list_operations_executed) = len(list_operations_chronology)
